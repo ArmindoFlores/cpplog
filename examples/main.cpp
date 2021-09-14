7e5212e92fc65c25c19d5ccaf22322caafcc4408
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2021-09-14 17:50:25 +0100

    Updated example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,16 +1,33 @@
 #include "cpplog.hpp"
+using namespace cpplog;
+
+void thread_worker()
+{
+    Logger::debug("Counting to 10...");
+    for (int i = 0; i < 10; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        Logger::info("i=" + std::to_string(i+1));
+    }
+    Logger::warning("Counting finished");
+}
 
 int main()
 {
-    cpplog::Logger logger(cpplog::LogLevel::INFO, "$thread_name: [%D %T] ($level) -> $message");
+    Logger::set_config_format("$thread_name: [%D %T] ($level) -> $message ");
+    Logger::set_log_level(LogLevel::DEBUG);
+    Logger::register_current_thread("main-thread");
 
-    logger.register_current_thread("main-thread");
+    Logger::info("Starting threads...");
 
-    logger.debug("This is an debug message");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    logger.info("This is an info message");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    logger.warning("This is an warning message");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    logger.error("This is an error message");
+    std::thread t1([](){ Logger::register_current_thread("thread-worker-1"); thread_worker(); });
+    std::thread t2([](){ Logger::register_current_thread("thread-worker-2"); thread_worker(); });
+    std::thread t3([](){ Logger::register_current_thread("thread-worker-3"); thread_worker(); });
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    Logger::info("Done!");
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    Logger::delete_instance();
 }
